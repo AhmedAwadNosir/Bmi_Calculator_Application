905b30bc100292249ace3e4d43924a21113c57af
AhmedAwadNosir <ahmedalprens98@gmail.com>
2023-04-15 18:35:22 +0200

    basic grid finished in UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(const BmiCalculator());
 }
 
- class BmiCalculator extends StatefulWidget {
+class BmiCalculator extends StatefulWidget {
   const BmiCalculator({super.key});
 
   @override
@@ -14,9 +14,102 @@ void main() {
 class _BmiCalculatorState extends State<BmiCalculator> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        
+        backgroundColor: const Color(0xFF23253A),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF23253A),
+          title: const Text("BMICALCULTOR"),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: const Color(0xFF313143),
+                    ),
+                    height: 210,
+                    width: 170,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: const Color(0xFF313143),
+                    ),
+                    height: 210,
+                    width: 170,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: const Color(0xFF313143),
+                ),
+                height: 200,
+                width: 375,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: const Color(0xFF313143),
+                    ),
+                    height: 210,
+                    width: 170,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: const Color(0xFF313143),
+                    ),
+                    height: 210,
+                    width: 170,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFE83D66),
+                  ),
+                  child: const Text(
+                    "CALCULAT",
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
